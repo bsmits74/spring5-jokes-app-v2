980255301fb6a5b7c2f8d135cb86aa911c1974a6
bsmits74 <superbassmits@gmail.com>
2022-01-25 21:20:41 +0100

    Inial version

---
+++ src/main/java/guru/springframework/ChuckQuotes/ChuckQuotesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.ChuckQuotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/ChuckQuotes/ChuckQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.ChuckQuotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
